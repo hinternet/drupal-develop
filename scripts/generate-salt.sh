475da35fd56d9ec122f698c8757fa37c0cfee9f9
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2020-02-26 18:41:23 +0100

    Migrate salt generator to PHP script on project lifecycle
    Signed-off-by: Dennis A. Torres Rodriguez <d70rr3s@gmail.com>

--- scripts/generate-salt.sh
+++ scripts/generate-salt.sh
@@ -1,5 +1,23 @@
-#!/usr/bin/env sh
-if [ ! -f "$HASH_SALT_FILE" ]; then
-  LC_ALL=C;
-  cat /dev/urandom | tr -dc 'a-zA-Z0-9-_' | fold -w 74 | head -n 1 > "$HASH_SALT_FILE";
-fi
+#!/usr/bin/env php
+<?php
+
+/**
+ * Setup hash-salt for Drupal.
+ *
+ * This script is invoked during project creation, is not intended for
+ * other purposes. Use with caution or you can get lockout of your
+ * Drupal site.
+ */
+
+$hash_salt_file = getenv('HASH_SALT_FILE');
+
+if (empty($hash_salt_file)) {
+  $hash_salt_file = dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . '.salt';
+}
+
+if (!file_exists($hash_salt_file)) {
+  $salt = password_hash(base64_encode((string) microtime()), PASSWORD_DEFAULT, ['salt' => random_bytes(22)]);
+  file_put_contents($hash_salt_file, $salt);
+  chmod($hash_salt_file, 0640);
+  echo "Hash salt generated and written on file ${hash_salt_file}!" . PHP_EOL;
+}
